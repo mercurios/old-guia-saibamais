73868fa7a0f4b1ff956796060c45fc6710382f0b
Mercurios Soluções <mercuriossolucoes@gmail.com>
2013-09-13 17:31:18 -0300

    Teste restaurante

--- application/views/restaurante/inicial-restaurante.php
+++ application/views/restaurante/inicial-restaurante.php
@@ -5,8 +5,17 @@
 <!-- Conteudo
 ================================================== -->
 <div id="conteudo" class="">
-
-	
+	<div class="topo_area_categoria">
+		<img src="<?php echo base_url(); ?>/assets/images/icone_restaurante_laranja.png" class="icone_area_categoria" alt="icone" />
+		<h1 class="titulo_area_categoria">Restaurantes</h1>
+	</div>
+	<!--Inicio da área de conteúdo esquerda-->
+	<div id="area_conteudo_esquerda" class="area_conteudo">
+    	
+    </div> <!--Fim da área de conteúdo esquerda-->
+    <!--Inicio da área de conteúdo direita-->
+    <div id="area_conteudo_direita" class="area_conteudo">
+    </div><!--Fim da área de conteúdo direita-->
 </div><!-- /conteudo -->
 
 <div id="publicidade_inferior" class="publicidade">
