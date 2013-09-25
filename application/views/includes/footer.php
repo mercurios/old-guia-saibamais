df41f9dc3b3d441a3a83da19a88ac57c20ceeb61
Mercurios Soluções <mercuriossolucoes@gmail.com>
2013-09-25 20:36:51 -0300

    Estilo e rodapé atualizados e novos icones adicionados.
    O rodapé está quase finalizado, faltam apenas os links para as redes
    sociais.

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -12,7 +12,7 @@
                     </div>
                     <div class="rodape_2">
                     	<div id="anuncie">
-                            <img src="" alt="icone"/>
+                            <img src="<?php echo base_url(); ?>/assets/images/icone_anuncie.png" alt="icone"/>
                             <h3>Anuncie</h3>
                             <p>Veja aqui as vantagens de anunciar no guia.</p>
 						</div>
@@ -26,36 +26,70 @@
                     </div>
                 </div>
                 <div id="rodape_3">
-                    <h1 class="h1_redape">O que se acha aqui?</h1>
+                    <h1 class="h1_rodape">O que se acha aqui?</h1>
                     <ul>
-                        <li	class="titulo_li_rodape"></li>
-                        <li></li>
+                        <li	class="titulo_li_rodape">Restaurantes</li>
+                        <li><a href="">Churrascarias</a></li>
+                        <li><a href="">Delivery</a></li>
+                        <li><a href="">Drive-thrur</a></li>
+                        <li><a href="">Rodízio</a></li>
+                        <li><a href="">Self-service</a></li>
                     </ul>
                     <ul>
-                        <li	class="titulo_li_rodape"></li>
-                        <li></li>
+                        <li	class="titulo_li_rodape">Lanchonetes</li>
+                        <li><a href=""></a></li>
                     </ul>
                     <ul>
-                        <li	class="titulo_li_rodape"></li>
-                        <li></li>
+                        <li	class="titulo_li_rodape">Bebidas</li>
+                        <li><a href="">Bares</a></li>
+                        <li><a href="">Botecos</a></li>
+                        <li><a href="">Cafeterias</a></li>
+                        <li><a href="">Lanchonetes</a></li>
+                        <li><a href="">Restaurantes</a></li>
                     </ul>
                     <ul>
-                        <li	class="titulo_li_rodape"></li>
-                        <li></li>
+                        <li	class="titulo_li_rodape">Passeio e lazer</li>
+                        <li><a href="">Praticar esportes</a></li>
+                        <li><a href="">Rotas para passeio</a></li>
+                        <li><a href="">Visitar locais</a></li>
                     </ul>
                     <ul>
-                        <li	class="titulo_li_rodape"></li>
-                        <li></li>
+                        <li	class="titulo_li_rodape">Estadias</li>
+                        <li><a href="">Albergues</a></li>
+                        <li><a href="">Chalés</a></li>
+                        <li><a href="">Hoteis</a></li>
+                        <li><a href="">Pousadas</a></li>
+                        <li><a href="">Privês</a></li>
+                        <li><a href="">Resortes</a></li>
                     </ul>
                     <ul>
-                        <li	class="titulo_li_rodape"></li>
-                        <li></li>
+                        <li	class="titulo_li_rodape">Entretenimento</li>
+                        <li><a href="">Cinemas</a></li>
+                        <li><a href="">Exposições</a></li>
+                        <li><a href="">Feiras e eventos</a></li>
+                        <li><a href="">Shows</a></li>
+                        <li><a href="">Teatros</a></li>
                     </ul>
                 </div>
                 <div id="rodape_4">
-                    <h1 class="h1_redape">Fique mais perto de nós!</h1>
+                    <h1 class="h1_rodape laranja">Fique mais perto de nós!</h1>
+                    <div class="rodape_4_sociais"><a href=""><img src="<?php echo base_url(); ?>/assets/images/icone_curtir.png" alt="Curtir" title="Curta nossa página no facebook"/></a></div>
+                    <div class="rodape_4_sociais"><a href=""><img src="<?php echo base_url(); ?>/assets/images/icone_tweet.png" alt="Twitter" title="Siga-nos no twitter"/></a></div>
+                    <div class="rodape_4_sociais"><a href=""><img src="<?php echo base_url(); ?>/assets/images/icone_gplus.png" alt="GPlus" title="Siga-nos no Google+"/></a></div>
+                    <div class="rodape_4_sociais"><a href=""><img src="<?php echo base_url(); ?>/assets/images/icone_youtube.png" alt="youtube" title="Inscreva-se no nosso canal"/></a></div>
+                    <div class="rodape_4_sociais"><a href=""><img src="<?php echo base_url(); ?>/assets/images/icone_instagram.png" alt="instagram" title="Curta nossa página no facebook"/></a></div>
                 </div>
-                <div id="rodape_5"></div>
+                <div id="rodape_5">
+                	<div id="rodape_5_direitos">
+                    	<p>Todos os direitos de marca, imagem e conteúdo
+estão reservados para a Mercurios.</p>
+                        <p>© Guia Saiba Mais - 2013</p>
+                    </div>
+                    <div id="rodape_5_mercurios">
+                    	<h3>Uma ferramenta da</h3>
+                        <img src="<?php echo base_url(); ?>/assets/images/m_mercurios.png" alt="Mercurios" />
+                    </div>
+                </div>
 			</div>
         </div>
         
