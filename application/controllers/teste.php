12f706020246d34bfc0a4e75678a62194b33b4d0
Tassiano Alencar <tassianomac@me.com>
2013-10-22 15:37:31 -0300

    Atualizado

---
+++ application/controllers/teste.php
@@ -0,0 +1,49 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Teste extends CI_Controller {
+
+	/**
+	 * bebida
+	 */
+
+	/* Construtor
+	=========================================================== */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{	
+		// Titulo da página
+		$seo['titulopag'] = "Bebidas | Guia Saiba Mais";
+
+		// Meta tags
+		$seo['meta'] = array(
+			array('name' => 'language', 'content' => 'pt-br'),
+			array('name' => 'description', 'content' => 'Descrição de lanchonetes'),
+			array('name' => 'keywords', 'content' => 'Key, key'),
+			array('name' => 'author', 'content' => 'Mercurios'),
+			array('name' => 'googlebot', 'content' => 'ALL'),
+			array('name' => 'robots', 'content' => 'ALL'),
+			array('name' => 'viewport', 'content' => 'width=device-width; initial-scale=1.0')
+		);
+
+		// Meta tags facebook
+		$seo['metaface'] = array(
+			array('name' => 'og:title', 'content' => 'bebidas | Guia Saiba Mais', 'type' => 'property'),
+			array('name' => 'og:type', 'content' => 'website', 'type' => 'property'),
+			array('name' => 'og:url', 'content' => base_url('bebida'), 'type' => 'property'),
+			array('name' => 'og:image', 'content' => 'img', 'type' => 'property'),
+		);
+
+		// Carrega o header
+		$this->load->view('includes/header', $seo);
+
+		// Carrega o conteudo
+		$this->load->view('teste/teste', $dados);
+
+		// Carrega o rodape
+		$this->load->view('includes/footer');
+	}
+}
